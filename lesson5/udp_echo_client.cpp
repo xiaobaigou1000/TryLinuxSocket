3358462b2ad19fedca5b04f6bc528fa459ad9520
xiaobaigou1000 <xiaobaigou2000@qq.com>
2020-10-22 17:12:43 +0800

    upd echo server(unfinished)

--- lesson5/udp_echo_client.cpp
+++ lesson5/udp_echo_client.cpp
@@ -1,6 +1,12 @@
-
+#include<iostream>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
 
 int main()
 {
+    int client_socket = socket(PF_INET,SOCK_DGRAM,IPPROTO_UDP);
+    sockaddr_in server_address{};
+    server_address.sin_family=AF_INET;
     
 }
